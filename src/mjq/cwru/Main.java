cbbf1b8d36ac10f729d72f4355990d0d84991159
JunqiMa <endlessm@163.com>
2016-08-20 17:46:44 -0400

    binary heap

--- src/mjq/cwru/Main.java
+++ src/mjq/cwru/Main.java
@@ -44,13 +44,54 @@ public class Main {
         }
 
         private boolean less(int i, int j) {
-            if (pq[i].compareTo(pq[j]) < 0)
-                return true;
-            else return false;
+            return pq[i].compareTo(pq[j]) < 0;
         }
     }
 
-    
+    public class MaxPQ<Key extends Comparable<Key>> {
+        private Key[] pq;
+        private int N;
+
+        public MaxPQ(int capacity) {
+            pq = (Key[]) new Comparable[capacity];
+        }
+
+        public void insert(Key key) {
+            pq[N++] = key;
+            swim(N - 1);
+        }
+
+        public Key delMax() {
+            Key max = pq[0];
+            exch(pq, 0, --N);
+            sink(0);
+            pq[N+1] = null;
+            return max;
+        }
+
+        private void swim(int k) {
+            while (k > 1 && less(k / 2, k)) {
+                exch(pq, k / 2, k);
+                k = k / 2;
+            }
+        }
+
+        private void sink(int k) {
+            while (2 * k < N) {
+                int j = 2 * k;
+                if (less(j, j + 1)) j++; // find the larger child to switch
+                if (!less(k, j)) break; //it means parent node is larger than both children
+                exch(pq, k, j);
+                k = j;
+            }
+
+        }
+
+        private boolean less(int i, int j) {
+            return pq[i].compareTo(pq[j]) < 0;
+        }
+
+    }
 
 
 }
